cc1459b8550e5641857407f91727de3784eff29c
Manan Parikh <manan7008@gmail.com>
2019-08-16 18:52:18 +0530

    Create RequestValidator.java

---
+++ src/main/java/com/flightmgr/validator/RequestValidator.java
@@ -0,0 +1,29 @@
+
+package com.flightmgr.validator;
+
+import com.flightmgr.model.SearchRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class RequestValidator {
+
+    public void validate(SearchRequest request){
+        if(Objects.isNull(request.getDate())){
+            throw new RuntimeException("Invalid Input. Date is mandatory param");
+        }
+        if(request.getFlightNumber().isPresent()){
+            if(request.getOrigin().isPresent() || request.getDestination().isPresent()){
+                throw new RuntimeException("Invalid Input. Origin destiation and flight number both cannot be searched");
+            }
+
+        }else if(request.getDestination().isPresent() && request.getOrigin().isPresent()){
+                if(request.getFlightNumber().isPresent()){
+                    throw new RuntimeException("Invalid input");
+                }
+        }else {
+            throw new RuntimeException("Invalid Inputs..");
+        }
+    }
+}
