6ec1ea058d688efd4e3aaad21d59445725ddc262
Manan Parikh <manan7008@gmail.com>
2019-08-16 18:50:19 +0530

    Add files via upload

---
+++ src/main/java/com/flightmgr/model/SearchRequest.java
@@ -0,0 +1,18 @@
+
+package com.flightmgr.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Data
+@Builder
+public class SearchRequest {
+
+    private Optional<String> origin;
+    private Optional<String> destination;
+    private Optional<String> flightNumber;
+    private LocalDate date;
+}
