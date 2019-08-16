3f40bf6900450628d437390baa00a6e94b863ede
Manan Parikh <manan7008@gmail.com>
2019-08-16 18:50:03 +0530

    Create FlightInfo.java

---
+++ src/main/java/com/flightmgr/model/FlightInfo.java
@@ -0,0 +1,24 @@
+package com.flightmgr.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class FlightInfo {
+    private String flightNumber;
+    private String carrier;
+    private String origin;
+    private LocalDateTime departure;
+    private String destination;
+    private LocalDateTime arrival;
+    private String aircraft;
+    private Long distance;
+    private String travelTime;
+    private String status;
+
+
+
+}
